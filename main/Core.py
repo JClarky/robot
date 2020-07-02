b6b14134d0b1813360240b27c1ec71b76a48b8ba
jayden124 <jayden260100@gmail.com>
2020-07-02 17:12:12 +1200

    Code updated

---
+++ main/Core.py
@@ -0,0 +1,82 @@
+import time
+import RPi.GPIO as GPIO
+
+########## GPIO Setup
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+
+########## Motor Variables
+motor_max_value = 100
+motor_min_value = 0
+pin_left_forward = 13
+pin_left_backward = 6
+pin_right_forward = 12
+pin_right_backward = 5
+GPIO.setup(pin_left_forward, GPIO.OUT)
+GPIO.setup(pin_left_backward, GPIO.OUT)
+GPIO.setup(pin_right_forward, GPIO.OUT)
+GPIO.setup(pin_right_backward, GPIO.OUT)
+lpf = GPIO.PWM(pin_left_forward, 1000) #Left Pin Forward
+lpf.start(0)
+lpb = GPIO.PWM(pin_left_backward, 1000) #Left Pin Backward
+lpb.start(0)
+rpf = GPIO.PWM(pin_right_forward, 1000) #Right Pin Forward
+rpf.start(0)
+rpb = GPIO.PWM(pin_right_backward, 1000) #Right Pin Backward
+rpb.start(0)
+
+########## LED Variables
+led_pin = 24
+led_state = False
+GPIO.setup(led_pin, GPIO.OUT)
+GPIO.output(led_pin, 0)
+
+def move(l, r):
+    global threshold
+    global lpf, lpb, rpf, rpb
+    if l < threshold and l > 0:
+        l = 0
+    elif l > threshold and l < 0:
+        l = 0
+    if r < threshold and r > 0:
+        r = 0
+    elif r > threshold and r < 0:
+        r = 0
+
+    if l > 0:
+        lpb.ChangeDutyCycle(0)
+        lpf.ChangeDutyCycle(l)
+    elif l < 0:
+        l = -l
+        lpf.ChangeDutyCycle(0)
+        lpb.ChangeDutyCycle(l)
+    else:
+        lpf.ChangeDutyCycle(0)
+        lpb.ChangeDutyCycle(0)
+    if r > 0:
+        rpb.ChangeDutyCycle(0)
+        rpf.ChangeDutyCycle(r)
+    elif r < 0:
+        r = -r
+        rpf.ChangeDutyCycle(0)
+        rpb.ChangeDutyCycle(r)
+    else:
+        rpf.ChangeDutyCycle(0)
+        rpb.ChangeDutyCycle(0)
+
+def led_on():
+    global led_pin
+    GPIO.output(led_pin, 1)
+
+def led_off():
+    global led_pin
+    GPIO.output(led_pin, 0)
+
+def left_turn():
+    move(-100, 100)
+    time.sleep(1)
+
+def right_turn():
+    move(100, -100)
+    time.sleep(1)
---
+++ main/LineFollowing.py
@@ -0,0 +1,133 @@
+# Imports
+import cv2
+import numpy as np
+import time
+import Core
+
+# Configure camera capture
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+def image():
+    global cap
+
+    gray_threshold_max = 255
+    gray_threshold_min = 70 # ALTER TO MAKE PERFECTOOOOO
+    block_size = 119
+    img_split = []
+    img_width = 640
+    img_height = 480
+    turning_margin = 10 # How far the line has to be from the center for it to adjust
+    # Image Setup
+    ret, img = cap.read()
+    # img = cv2.flip(img, 0) # vert
+    # img = cv2.flip(img, 1) # horz
+
+    # Height , width
+    img = img[0:200, 100:540]
+    img_width = img.shape[1]
+    img_height = img.shape[0]
+
+    # split image
+    div_img_value = int(img_width / 4)
+    left_img = img[1:img_height, 0:div_img_value]
+    mid_img = img[1:img_height, div_img_value:div_img_value * 3]
+    right_img = img[1:img_height, div_img_value * 3:div_img_value * 4]
+    img_split = [left_img, mid_img, right_img]
+    img_split_contours = []
+    lines = [0, 0, 0]
+    forward_line = 0
+
+    # edges for split images
+    for f in range(0, 3):
+        i = img_split[f]
+        # Add gray scale effect
+        gray = cv2.cvtColor(i, cv2.COLOR_BGR2GRAY)
+        # Add blur effect
+        blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+        # Add threshold effect
+        rt, threshold = cv2.threshold(blurred, gray_threshold_min, gray_threshold_max, cv2.THRESH_BINARY_INV)
+        # Detect lines of the image
+        contours, hierarchy = cv2.findContours(threshold.copy(), 1, cv2.CHAIN_APPROX_NONE)
+        for i in contours:
+            lines[f] = lines[f] + 1
+        try:
+            # If there are any lines
+            if len(contours) > 0:
+                c = max(contours, key=cv2.contourArea) # Find biggest contour
+                M = cv2.moments(c) # Center of that contour
+                cx = int(M['m10'] / M['m00']) # X coordiantes contour
+                cy = int(M['m01'] / M['m00']) # Y coordiantes contour
+                cv2.line(i, (cx, 0), (cx, 720), (255, 0, 0), 1) # Create line around x axis of contour
+                cv2.line(i, (0, cy), (1280, cy), (255, 0, 0), 1) # Create line around y axis of contour
+                cv2.drawContours(i, contours, -1, (0, 255, 0), 1) # Draw the lines
+                if f == 1:
+                    forward_line = cx
+            else:
+                pass
+        except:
+            print("failure")
+
+    # Add gray scale effect
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Add blur effect
+    blurred = cv2.GaussianBlur(gray, (9, 9), 0)
+    # Add threshold effect
+    rt, threshold = cv2.threshold(blurred, gray_threshold_min, gray_threshold_max, cv2.THRESH_BINARY_INV)
+    # Detect lines of the image
+    contours, hierarchy = cv2.findContours(threshold.copy(), 1, cv2.CHAIN_APPROX_NONE)
+
+    try:
+        # If there are any lines
+        if len(contours) > 0:
+            c = max(contours, key=cv2.contourArea) # Find contour with biggest area
+            M = cv2.moments(c) # Center of that contour
+            cx = int(M['m10'] / M['m00']) # X coordiantes contour
+            cy = int(M['m01'] / M['m00']) # Y coordiantes contour
+            cv2.line(img, (cx, 0), (cx, 720), (255, 0, 0), 1) # Create line around x axis of contour
+            cv2.line(img, (0, cy), (1280, cy), (255, 0, 0), 1) # Create line around y axis of contour
+            cv2.drawContours(img, contours, -1, (0, 255, 0), 1) # Draw the lines
+        else:
+            return('no')
+    except:
+        print("failure")
+
+    # Captured images in frames
+    #cv2.imshow('raw_video', img)
+    #cv2.imshow('gray_scale', gray)
+    #cv2.imshow('threshold', threshold)
+    #cv2.imshow('left', left_img)
+    #cv2.imshow('mid', mid_img)
+    #cv2.imshow('right', right_img)
+
+    # Figure out robot direction
+    center_pixel = img_width/4 # Find center x pixel
+    print(forward_line)
+    if forward_line > center_pixel:
+        if forward_line-center_pixel > turning_margin:
+            return('right')
+    elif forward_line < 10:
+        return('no')
+    else:
+        if center_pixel-forward_line > turning_margin:
+            return('left')
+
+Core.run = False
+Core.move(50, 50)
+
+while True:
+    time.sleep(0.1)
+    command = image()
+    if command == 'left':
+        print('left')
+        Core.move(70, 80)
+    elif command == 'right':
+        print('right')
+        Core.move(100, 50)
+    elif command == 'no':
+        print("Find line")
+        Core.move(100,-80)
+    else:
+        print('straight')
+        Core.move(80, 65)
